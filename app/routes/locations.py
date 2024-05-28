b2c7cc318f4ce98ba304a7c3a3814e6337b2290c
Piotr Pyka <pyka.piotr.00@gmail.com>
2024-05-28 09:20:49 +0000

    Add location_overview and location_details views, use lateral join

--- app/routes/locations.py
+++ app/routes/locations.py
@@ -20,13 +20,8 @@ async def list_locations(request: Request,
         locations = await connection.fetch(
             """
             SELECT 
-                id, 
-                name, 
-                seats as max_seats,
-                image_url,
-                description, 
-                coordinates
-            FROM "location" 
+                *
+            FROM location_overview
             ORDER BY id LIMIT $1 OFFSET $2
             """, limit, offset
         )
@@ -50,37 +45,10 @@ async def get_events(request: Request,
     async with db.acquire() as connection:
         location = await connection.fetchrow(
             """
-            WITH closest_event AS (
-                SELECT
-                    event.id,
-                    event.name,
-                    event.start_date,
-                    event.seats as seats_left,
-                    event.image_url,
-                    event.description
-                FROM
-                    event
-                WHERE event.location_id = $1
-                AND current_date < event.start_date
-                ORDER BY start_date ASC LIMIT 1
-            )
-
             SELECT 
-                location.id, 
-                location.name,
-                location.image_url, 
-                seats as max_seats, 
-                ST_Y(ST_AsText(location.coordinates::geometry)) as latitude,
-                ST_X(ST_AsText(location.coordinates::geometry)) as longitude,
-                location.description,
-                closest_event.name as closest_event_name,
-                closest_event.start_date as closest_event_start_date,
-                closest_event.seats_left as closest_event_seats_left,
-                closest_event.image_url as closest_event_image_url,
-                closest_event.description as closest_event_description,
-                closest_event.id as closest_event_id
-            FROM "location", closest_event 
-            WHERE location.id = $1
+                *
+            FROM location_details
+            WHERE location_details.id = $1
             """, location_id
         )
     if response_type == "json":
--- sql/05_add_views.sql
+++ sql/05_add_views.sql
@@ -44,3 +44,49 @@ CREATE OR REPLACE VIEW event_details AS
         location l ON e.location_id = l.id 
     LEFT JOIN 
         artist_aggregation aa ON aa.event_id = e.id;
+
+-- views for querying location
+CREATE OR REPLACE VIEW location_overview AS 
+    SELECT 
+        id, 
+        name, 
+        seats as max_seats,
+        image_url,
+        description, 
+        coordinates
+    FROM "location"; 
+
+CREATE VIEW location_details AS
+    SELECT 
+        location.id, 
+        location.name,
+        location.image_url, 
+        location.seats AS max_seats, 
+        ST_Y(ST_AsText(location.coordinates::geometry)) AS latitude,
+        ST_X(ST_AsText(location.coordinates::geometry)) AS longitude,
+        location.description,
+        closest_event.name AS closest_event_name,
+        closest_event.start_date AS closest_event_start_date,
+        closest_event.seats AS closest_event_seats_left,
+        closest_event.image_url AS closest_event_image_url,
+        closest_event.description AS closest_event_description,
+        closest_event.id AS closest_event_id
+    FROM 
+        location
+    LEFT JOIN LATERAL (
+        SELECT 
+            event.id,
+            event.name,
+            event.start_date,
+            event.seats,
+            event.image_url,
+            event.description
+        FROM 
+            event
+        WHERE 
+            event.location_id = location.id
+            AND current_date < event.start_date
+        ORDER BY 
+            event.start_date ASC
+        LIMIT 1
+    ) closest_event ON true;
